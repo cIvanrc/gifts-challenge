f7997205210e9785d4849318ffb36a342d47377c
ivan <c.ivan.felix.r@gmail.com>
2021-04-17 08:11:51 -0500

    added User model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence (:email) { |n| 'example_email_#{n}@domain.com' }
+    sequence (:email) { |n| "example_email_#{n}@domain.com" }
     password { 'password' }
   end
 end
